31e6fb51b77567bf7d5000643e78871de6d952df
souf139 <soufianboushaba12@gmail.com>
2023-12-13 09:00:56 +0100

    this commit is for creating the update

---
+++ update.php
@@ -0,0 +1,17 @@
+<?php
+include 'db.php';
+include 'article.php';
+include 'art.php';
+
+$db = new DB();
+$articleManager = new Articlee($db->connect());
+
+$article = new Articlee();
+$article->id = $_POST['article_id'];
+$article->titre = $_POST['nouveau_titre'];
+$article->contenu = $_POST['nouveau_contenu'];
+
+$articleManager->updateArticle($article);
+
+header("Location: index.php");
+?>
---
+++ user.php
@@ -0,0 +1,29 @@
+<?php  
+
+
+class user{
+
+    private $db;
+
+    public function __construct() {
+        $this->db = new PDO("mysql:host=localhost;dbname=article", "root", "");
+       
+    }
+    public function Registre($firstname,$lastname,$username,$password,$email, $role_id){
+        $query = "INSERT INTO utilisateur (`firstname`, `lastname`, `username`, `password`, `email`, `role_id`) VALUES (?, ?, ?, ?,?,?)";
+        $stmt = $this->db->prepare($query);
+        $stmt->execute([$firstname,$lastname,$username,$password,$email, $role_id]);
+        return true;
+    }
+
+    public function Login($email,$password){
+       
+        $stmt=$this->db->prepare("SELECT * FROM utilisateur WHERE username=? AND password=?");
+        $result=$stmt->execute([$email,$password]);
+        $numRows = $stmt->rowCount();
+        if($result && $numRows>0){
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            return $row;
+        }
+    }
+}
