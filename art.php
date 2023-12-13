b5f70a6bb0c193cd5d2098e5fa8f63dea80c3a0f
souf139 <soufianboushaba12@gmail.com>
2023-12-13 08:58:18 +0100

    this commit is for creating the art

---
+++ art.php
@@ -0,0 +1,40 @@
+<?php class Article {
+    public $id;
+    public $titre;
+    public $contenu;
+    public $date_de_creation;
+    public $user_id;
+}
+
+?>
+
+<!-- 
+
+class Role {
+    public $id;
+    public $titre;
+    public $description;
+}
+
+class Utilisateur {
+    public $id;
+    public $firstname;
+    public $lastname;
+    public $username;
+    public $password;
+    public $email;
+    public $role_id;
+}
+
+class Administrateur {
+    public $id;
+    public $firstname;
+    public $lastname;
+    public $username;
+    public $password;
+    public $email;
+    public $phone;
+    public $cin;
+    public $role_id;
+}
+ -->
